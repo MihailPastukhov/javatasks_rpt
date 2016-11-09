d31aad34aa261b51ee334eb8a9f3a22349c02197
MihailPastukhov <michael6989@gmail.com>
2016-11-09 21:44:08 +0300

    В тесты модификации и добавления групп добавлены проверки списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModifyTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModifyTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -21,11 +22,16 @@ public class GroupModifyTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initModificationGroup();
-        app.getGroupHelper().fillGroupData(new GroupData("test4", "test5", "test6"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(), "test4", "test5", "test6");
+
+        app.getGroupHelper().fillGroupData(group);
         app.getGroupHelper().updateGroupData();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new  HashSet<Object>(after));
     }
 }
